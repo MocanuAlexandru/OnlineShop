e7e456c04c44837d00642c49cd331aa05a3e097b
MocanuAlexandru <36726395+MocanuAlexandru@users.noreply.github.com>
2022-03-23 14:00:05 +0200

    Initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Proiect.Models
+{
+    public class Product
+    {
+        [Key]
+        public int productId;
+        [Required]
+        public String title;
+        [Required]
+        public String description;
+        public String image;
+        [Required]
+        public Double price;
+    }
+}
